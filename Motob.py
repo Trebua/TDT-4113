ae49728cc8171649f112af573d3e46eff11a4d6c
trebua <31652936+trebua@users.noreply.github.com>
2017-11-02 12:26:08 +0100

    text

--- Motob.py
+++ Motob.py
@@ -1,7 +1,13 @@
+#from basic_robot.motors import *
+import basic_robot.motors.py
+
 class motob():
 
     motors = [] # a list of the motors whose settings will be determined by the motob.
     value = None #a holder of the most recent motor recommendation sent to the motob.
+
+    def __init__(self):
+        return
 
     def update(self,recommendation):# - receive a new motor recommendation, load it into the value slot, and operationalize it.
         return True
