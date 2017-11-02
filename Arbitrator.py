39319ca142fd7c238775f57b85399d925b406fd3
hermanro77 <33314755+hermanro77@users.noreply.github.com>
2017-11-02 13:11:26 +0100

    ..

--- Arbitrator.py
+++ Arbitrator.py
@@ -1,12 +1,14 @@
 class Arbitrator():
 
 
-    def __init__(self, bbcon):
+    def __init__(self, bbcon, stochastic):
         self.bbcon = bbcon
+        self.stochastic = stochastic
 
 
     def choose_action(self):
-        active_behaviours = self.bbcon.activate_behavior()
+        active_behaviours = self.bbcon.active_behaviors
+
         return True
 
     def stochastic_choice(self):
