fb1e142117a6d3a73364ca875a415fd9e3328ac6
trebua <31652936+trebua@users.noreply.github.com>
2017-11-02 13:31:03 +0100

    endirnger

--- BBCON.py
+++ BBCON.py
@@ -9,7 +9,6 @@ class BBCON():
     #Other instance variables, such as the current timestep, the inactive behaviors, and the controlled agent/robot
     #may also prove useful in your implementation.
 
-
     def add_behavior(self, behavior): #append a newly-created behavior onto the behaviors list.
         self.behaviors.append(behavior)
         return 
@@ -27,16 +26,28 @@ class BBCON():
             self.active_behaviors.remove(behavior)
         return
 
+    def update_sensobs(self):
+        for sensor in self.sensobs:
+            sensor.update()
+
+    def choose_winning_behaviour(self):
+        winner = self.arbitrator.choose_action()
+        return winner.motor_recommendations[0] #Er det riktig at første recommendation skal velges? eventuelt fjerne denne behaviouren?
+        #Skal man ha med noe om activeflag her eller håndteres det i behaviour?
+
 
     def run_one_timestep(self):
         #1. Update all sensobs - These updates will involve querying the relevant sensors
         # for their values, along with any pre-processing of those values (as described below)
+        self.update_sensobs()
+
 
         #2. Update all behaviors - These updates involve reading relevant sensob values and
         #  producing a motorrecommendation.
 
         #3. Invoke the arbitrator by calling arbitrator.choose action, which will choose
         #  a winning behavior andreturn that behavior’s motor recommendations and halt request flag.
+        recommendation = self.choose_winning_behaviour()
 
         #4. Update the motobs based on these motor recommendations. The motobs will then update
         #  the settings of all motors.
