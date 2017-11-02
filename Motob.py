0788a30da96c827aa4c54497aeddafb8c90aacb0
trebua <31652936+trebua@users.noreply.github.com>
2017-11-02 12:47:45 +0100

    Motob  - mangler import + håndtering av vinkel

--- Motob.py
+++ Motob.py
@@ -7,12 +7,21 @@ class motob():
     value = None #a holder of the most recent motor recommendation sent to the motob.
 
     def __init__(self):
+        #Legg til motorobjekt i motors
         return
 
     def update(self,recommendation):# - receive a new motor recommendation, load it into the value slot, and operationalize it.
-        return True
+        self.value = recommendation
+        self.operationalize()
 
+    #Vil få verdi som feks ((L,45), True/False)
+    #Regner med at motors kun inneholder et motors-objekt
     def operationalize(self):
-        # convert a motor recommendation into one or more motor settings, which are sent to
-        #the corresponding motor(s).
-        return True
+        if not self.value[1]:
+            self.motors[0].stop()
+        else:
+            if self.value[0][0] == "L":
+                self.motors[0].setValue([-1,1],1) #Detaljer senere: hvordan få til 45 grader?
+            else:
+                self.motors[0].setValue([1,-1],1)
+
