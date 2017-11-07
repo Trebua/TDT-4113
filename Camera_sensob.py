114eaefa36ff6915f3d05570019582229031d313
Jorgen Sund <sundjorgen@gmail.com>
2017-11-07 11:20:30 +0100

    Camera Sensob

--- Camera_sensob.py
+++ Camera_sensob.py
@@ -4,7 +4,9 @@ from basic_robot import camera
 class Camera_sensob(Sensob):
     def __init__(self):
         Sensob.__init__(Sensob)
-        self.camera = camera.Camera()
+        self.img_width = 128
+        self.img_height = 96
+        self.camera = camera.Camera(img_width=self.img_width,img_height=self.img_height)
         self.value = None
 
     def update(self):
