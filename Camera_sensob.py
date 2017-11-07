fc65a840dea4d8b2df50ae6dd2a0e17781fb827c
Jorgen Sund <sundjorgen@gmail.com>
2017-11-07 11:43:16 +0100

    Camera Sensob

--- Camera_sensob.py
+++ Camera_sensob.py
@@ -1,5 +1,6 @@
 import Sensob.py
 from basic_robot import camera
+from PIL import Image
 
 class Camera_sensob(Sensob):
     def __init__(self):
@@ -8,12 +9,26 @@ class Camera_sensob(Sensob):
         self.img_height = 96
         self.camera = camera.Camera(img_width=self.img_width,img_height=self.img_height)
         self.value = None
+        self.image = None
         self.min_green = 150 # Thresholds for the color testing
         self.max_red = 150 #Change these as needed
         self.max_blue = 150
 
     def update(self):
-        self.value = self.camera.update()
+        self.image = self.camera.update()
+        self.value = self.find_green()
         return self.value
 
     def find_green(self):
+        amount = 0
+        total_width = 0
+        for width in range(self.img_width):
+            for height in range(self.img_height):
+                if self.check_pixel(self.image(width,height)):
+                    total_width+=width
+                    amount += 1
+        return total_width/amount
+
+    def check_pixel(self,pixel):
+        return(pixel[0] < self.max_red and pixel[1] > self.min_green and pixel[2] < self.max_blue)
+
