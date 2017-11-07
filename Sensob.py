9fd2096fa0645975229f71a9e3a87b5e8543d4db
trebua <31652936+trebua@users.noreply.github.com>
2017-11-07 13:43:03 +0100

    kommentz

--- Sensob.py
+++ Sensob.py
@@ -1,11 +1,14 @@
 from abc import abstractmethod
 
+#Superklasse for sensorobjekter
 class Sensob:
 
+    #Alle sensorer har en verdi mellom 0-1 som behandles i behaviour + arbitrator
     def __init__(self, sensors):
         self.value = None
         return
 
+    #Metode alle sensobs skal ha, som oppdaterer sensorverdiene og finner ut ny self.value
     @abstractmethod
     def update(self):
         return
