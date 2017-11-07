d4294236dd43ff9c6e11fbc34f755036cfc7da65
Jorgen Sund <sundjorgen@gmail.com>
2017-11-07 11:33:05 +0100

    Camera Sensob

--- Camera_sensob.py
+++ Camera_sensob.py
@@ -8,9 +8,12 @@ class Camera_sensob(Sensob):
         self.img_height = 96
         self.camera = camera.Camera(img_width=self.img_width,img_height=self.img_height)
         self.value = None
+        self.min_green = 150 # Thresholds for the color testing
+        self.max_red = 150 #Change these as needed
+        self.max_blue = 150
 
     def update(self):
         self.value = self.camera.update()
         return self.value
 
-
+    def find_green(self):
